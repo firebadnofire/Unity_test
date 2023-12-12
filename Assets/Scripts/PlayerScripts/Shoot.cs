a37b4ea77fbbbe73bb141ee37de3cfc36666751a
firebadnofire <william@archuser.org>
2023-12-12 08:46:18 -0500

    commit

---
+++ Assets/Scripts/PlayerScripts/Shoot.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+ 
+public class Shoot : MonoBehaviour {
+ 
+    public Rigidbody projectile;
+ 
+    public float speed = 20;
+ 
+ 
+ 
+    // Use this for initialization
+    void Start () {
+   
+    }
+   
+    // Update is called once per frame
+    void Update () {
+ 
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Rigidbody instantiatedProjectile = Instantiate(projectile,
+                                                           transform.position,
+                                                           transform.rotation)
+                as Rigidbody;
+ 
+            instantiatedProjectile.velocity = transform.TransformDirection(new Vector3(0, 0, speed));
+ 
+        }
+    }
+}
