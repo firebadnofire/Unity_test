235181054b9c2ece6794de23a02f2f66aa9e5cc2
firebadnofire <william@archuser.org>
2024-02-06 08:44:34 -0500

    commit

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -12,7 +12,7 @@ public class Controller : MonoBehaviour
     public float gravity = 20.0f;
     public Camera playerCamera;
     public float lookSpeed = 2.0f;
-    public float lookXLimit = 45.0f;
+    public float lookXLimit = 90.0f;
 
     CharacterController characterController;
     Vector3 moveDirection = Vector3.zero;
